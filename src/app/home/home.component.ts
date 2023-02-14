b2dc85348bf9c715ce873a8fdf3a86d5729369af
duongthanhan <dtabookin@gmail.com>
2023-02-14 15:23:50 +0700

    bai 16 learn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  constructor() {}
 
   public name ='Duong Thanh An';
   public age = 15;
@@ -20,19 +22,60 @@ export class HomeComponent implements OnInit {
     ten: 'Coconut',
     gia: 12900,
     giaKhuyenMai: 10900,
-    haGia: false
+    haGia: true
   },{
     ten: 'Banana',
-    gia: 23,
-    giaKhuyenMai: 20
+    gia: 23000,
+    giaKhuyenMai: 23000,
+    haGia: false
   }];
 
-  constructor() {}
-  ngOnInit(): void {
-    console.log(`traiCay = `,this.traiCay);
-  }
+  public districts:string[] = [];
+
+  public VietNamCity = [
+    {
+      city: 'Chọn thành phố', 
+      district: [] 
+    },
+    {
+      city: 'Long An', 
+      district: ["Long An","TP Tân An","Bến Lức","Tân Trụ","Thủ Thừa"] 
+    },
+    {
+      city: 'Hồ Chí Minh', 
+      district: [
+        'Gò vấp',
+        'Q1',
+        'Q2',
+        'Q3',
+        'Q4']
+    }
+  ];
 
   public resetName() :void{
     this.name = '';
   }
+
+  
+  ngOnInit(): void {
+    console.log('cities = ',this.VietNamCity);
+    
+  }
+
+  public changeCity(event :any){
+    const chosenCity = event.target.value;
+    if(!chosenCity)
+      return;
+
+    //cach 1
+    // const search = this.VietNamCity.filter((city) => city.city === chosenCity);
+    // if(search && search.length > 0)
+    //   this.districts = search[0].district;
+    
+
+    //cach 2
+    this.districts = this.VietNamCity.find(data => data.city === chosenCity)?.district || [];
+  }
+
+
 }
