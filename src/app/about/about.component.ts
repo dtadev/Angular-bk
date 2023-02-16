ce531f2a2d5c1aca638d6cc51d729a87375d8926
duongthanhan <dtabookin@gmail.com>
2023-02-16 10:21:52 +0700

    asdlas

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
+<<<<<<< HEAD
 export class AboutComponent implements OnInit {
   public loginName = 'admin';
+=======
+export class AboutComponent {
+>>>>>>> parent of 532cb71 (learn ngswitch)
 
-  constructor(){
-
-  }
-  ngOnInit(): void {
-    
-  }
 }
